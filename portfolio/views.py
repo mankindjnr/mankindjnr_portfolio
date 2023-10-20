ec3475e354056e767f3c5ddc21c843926fcd7ea5
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-10-20 16:30:38 +0300

    portfolio

---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'portfolio/index.html')
+
+def resume(request):
+    return render(request, 'portfolio/resume.html')
