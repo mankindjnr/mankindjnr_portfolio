40ed62fdc8cf57ad51c169ba114e82c614a803c8
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-05-23 04:23:22 +0300

    updated resume

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html')
+    return render(request, "portfolio/index.html")
+
 
 def resume(request):
-    return render(request, 'portfolio/resume.html')
+    return render(request, "portfolio/resume.html")
